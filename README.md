f237f7e848595ed5691060ccf3cf965b9deaff8a
solomon911 <solomonsahayaraj911@gmail.com>
2020-10-21 12:57:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-ge9p2u
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-ge9p2u)
---
+++ index.ts
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+console.log(JSON.stringify(undefined))
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
